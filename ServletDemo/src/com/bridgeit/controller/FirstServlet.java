7b2a44e0bfce00e73b72138965984dbcb539b7e2
sowndar18 <sowndar1802@gmail.com>
2018-12-05 19:00:23 +0530

    login from

--- ServletDemo/src/com/bridgeit/controller/FirstServlet.java
+++ ServletDemo/src/com/bridgeit/controller/FirstServlet.java
@@ -1,15 +1,22 @@
 package com.bridgeit.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
-import javax.servlet.GenericServlet;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class FirstServlet extends GenericServlet{
+public class FirstServlet extends HttpServlet{
 
-	@Override
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	String name=	request.getParameter("nm");
@@ -20,5 +27,37 @@ public class FirstServlet extends GenericServlet{
 	printWriter.flush();
 	printWriter.close();
 	}
-
+*/
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		  response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	 
+	        String n = request.getParameter("firstname");
+	        String p = request.getParameter("lastname");
+	        String e = request.getParameter("email");
+	        String c = request.getParameter("password");
+	 
+	        try {
+	            Class.forName("com.mysql.jdbc.Driver");
+	           Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/college","root","root");
+	    		
+	            PreparedStatement ps = con
+	                    .prepareStatement("insert into  servlet(firstname,lastname,email,password) values(?,?,?,?)");
+	 
+	            ps.setString(1, n);
+	            ps.setString(2, p);
+	            ps.setString(3, e);
+	            ps.setString(4, c);
+	 
+	            int i = ps.executeUpdate();
+	            if (i > 0)
+	                out.print("You are successfully registered...");
+	 
+	        } catch (Exception e2) {
+	            System.out.println(e2);
+	        }
+	 
+	        out.close();
+	}
+	
 }
